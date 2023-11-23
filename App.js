ed67ec9f2857fc3d08b5e853e43c59ebe2f7e491
Euler-JS <joaoalbertojose96@gmail.com>
2023-11-23 19:41:26 +0200

    Criando estilos, view e text e stylesheet

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+
+//view -> container that will wrap multiple components
+//text -> to showcase any text
+
+import { StyleSheet, Text } from "react-native";
+import { View } from "react-native";
 
 export default function App() {
-  return (
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <View style={styles.nestedContainer}>
+          <Text>Nested Test</Text>
+        </View>
+        <Text style={{
+          fontSize: 20,
+          margin: 30,
+          color: 'white'
+        }}> Hello React</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    display: 'flex',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'red'
   },
-});
+  nestedContainer: {
+    display: 'flex',
+    width: 200,
+    height: 200,
+    backgroundColor: 'green',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
