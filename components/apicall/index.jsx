a7e56928bdab30218c17abce47c1416d2d7bc9ba
Euler-JS <joaoalbertojose96@gmail.com>
2023-11-24 13:52:58 +0200

    loading state ao carregar dados

--- components/apicall/index.jsx
+++ components/apicall/index.jsx
@@ -1,31 +1,36 @@
 import { useEffect, useState } from "react";
-import { FlatList, Text, View } from "react-native";
+import { ActivityIndicator, FlatList, Text, View } from "react-native";
 
 // Data state
 
-export default function ApiCall(){
+export default function ApiCall() {
+    [loading, setLoading] = useState(true)
     [apiData, setApiData] = useState([])
-
-    useEffect(()=> {
-    async function getDataFromApi() {
-        const apiResponse = await fetch('https://dummyjson.com/users')
-        const finalData = await apiResponse.json()
-        // setApiData(finalData.users.map(userItem=> userItem.age));
-        setApiData(finalData.users.map(userItem=> `${userItem.firstName} ${userItem.lastName} ${userItem.age}`));
-    }
-    getDataFromApi()
+    useEffect(() => {
+        console.log("Loading ", loading);
+        async function getDataFromApi() {
+            const apiResponse = await fetch('https://dummyjson.com/users')
+            const finalData = await apiResponse.json()
+            // setApiData(finalData.users.map(userItem=> userItem.age));
+            if (finalData) {
+                setApiData(finalData.users.map(userItem => `${userItem.firstName} ${userItem.lastName} ${userItem.age}`));
+                setLoading(false)
+            }
+        }
+        getDataFromApi()
     }, [])
-
-    console.log("Data: ", apiData)
-
-
-    return(
+    if (loading) {
+        return (
+            <ActivityIndicator color={'red'} size={'large'} />
+        )
+    }
+    return (
         <View>
             <Text>Api Data </Text>
             <View>
-                <FlatList 
-                data={apiData}
-                renderItem={(itemData)=> <Text>{itemData.item}</Text>}/>
+                <FlatList
+                    data={apiData}
+                    renderItem={(itemData) => <Text>{itemData.item}</Text>} />
             </View>
         </View>
     )
